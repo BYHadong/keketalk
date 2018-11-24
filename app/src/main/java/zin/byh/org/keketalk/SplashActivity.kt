146288e607a932fc7ec8801e92ee604d717396c0
YoungChan <byou0070@gmail.com>
2018-11-24 22:07:50 +0900

    디자인 변경

--- app/src/main/java/zin/byh/org/keketalk/SplashActivity.kt
+++ app/src/main/java/zin/byh/org/keketalk/SplashActivity.kt
@@ -1,9 +1,15 @@
 package zin.byh.org.keketalk
 
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.v7.app.AlertDialog
+import android.util.Log
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -12,22 +18,37 @@ import zin.byh.org.keketalk.Util.SharedPreferenceUtil
 
 class SplashActivity : AppCompatActivity() {
 
-    val SPLASH_ACTIVITY_LODING_TIME: Long = 500
+    val TAG = "SplashActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed(object : Runnable {
-            override fun run() {
-                val name = SharedPreferenceUtil.getPreference(this@SplashActivity)
-                if (name != "") {
-                    startActivity(Intent(this@SplashActivity, ChatActivity::class.java))
-                    finish()
-                } else {
-                    startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-                    finish()
-                }
-            }
-        }, SPLASH_ACTIVITY_LODING_TIME)
+        val networkCheck = getNetworkCheck()
+        Log.d(TAG, (networkCheck != null && networkCheck.isConnected).toString())
+        //네트워트 상태 체크
+        if (networkCheck != null && networkCheck.isConnected) {
+            val name = SharedPreferenceUtil.getPreference(applicationContext)
+            Handler().postDelayed({
+                if (name.equals(""))
+                    startActivity(Intent(applicationContext, LoginActivity::class.java))
+                else
+                    startActivity(Intent(applicationContext, ChatActivity::class.java))
+                finish()
+            },500)
+        } else {
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle("네트워크 연결 상태 확인")
+            alertDialog.setMessage("네트워크를 연결하고 다시 시행시켜 주세요.")
+            alertDialog.setPositiveButton("확인",  { dialog, which ->
+                finish()
+            })
+            alertDialog.show()
+        }
+    }
+
+    fun getNetworkCheck(): NetworkInfo? {
+        val connectivityManager = baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCheck = connectivityManager.activeNetworkInfo
+        return networkCheck
     }
 }
